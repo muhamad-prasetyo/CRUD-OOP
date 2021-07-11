4e9d2813819603e2cff6109b764f90b628ec5f06
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-11 16:29:53 +0700

    menambahkan sebuah kondisi dari submit_edit

--- proses.php
+++ proses.php
@@ -17,4 +17,20 @@ if (isset($_POST['submit_simpan'])) {
     header('Location:index.php');
 }
 
+// cek apakah terdapat data/objeck masuk dari POST submit_edit 
+if(isset($_POST['submit_edit'])) {
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $uts = $_POST['uts'];
+    $uas = $_POST['uas'];
+    $tugas = $_POST['tugas'];
+    
+    $model = new Model();
+
+    // kemudian variable diatas di parsing kedalam method update pada class model
+    $model->update($nim,$nama,$uts,$tugas,$uas);
+
+    header('Location:index.php');
+}
+
 // isset() adalah salah satu perintah php yang berfungsi untuk memeriksa sebuah objek dari form, apakah ada atau tidak nilai yang di lempar dari suatu form. Fungsi "isset() akan tetap bernilai true meskipun nilai dari suatu form tidak terisi atau kosong. Namun untuk nilai false atau sebaliknya bisa ditambahkan operator ! pada isset() tersebut, sehingga menjadi !isset()." 
