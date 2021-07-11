894604b2ecc51a530313fe245078109630922fa1
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-11 10:32:44 +0700

    Menambahkan method insert, na dan status

--- model.php
+++ model.php
@@ -9,4 +9,34 @@ class Model extends Connection {
     {
         $this->conn = $this->get_connection();
     }
+
+    // method untuk memasukan data ke dalam mysql database
+    public function insert($nim, $nama, $uts, $uas, $tugas)
+    {
+        $na = $this->na($uts,$tugas,$uas);
+        $status = $this->status($na);
+        $sql = "INSERT INTO tbl_nilai (nim, nama, uts, uas, tugas, na, status) VALUES ('$nim', '$nama', '$uts', '$uas', '$tugas', '$na', '$status')";
+
+        $this->conn->query($sql);
+    }
+
+    public function na($uts,$tugas,$uas) 
+    {
+        $na =(0.3*$uts)+(0.3*$tugas)+(0.4+$uas);
+        
+        return $na;
+    }
+
+    public function status($na) 
+    {
+        $status="";
+
+        if($na >= 60 && $na <=100) {
+            $status="LULUS";
+        } else {
+            $status="TIDAK LULUS";
+        }
+
+        return $status;
+    }
 }
